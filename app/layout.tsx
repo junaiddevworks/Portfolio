31e59d0ad725012354f1452e15fe31fb457ab894
JunaidSFT <78009418+JunaidSFT@users.noreply.github.com>
2024-10-02 21:30:52 +0500

    added dot in projects

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/mj.png" sizes="any" />
+        <link rel="icon" href="./mj.png" sizes="any" />
       </head>
       <body className={inter.className}>
         <ThemeProvider
