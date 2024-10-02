1ef42d2b67d051029531e13d7b6fd10ddfc918a5
JunaidSFT <78009418+JunaidSFT@users.noreply.github.com>
2024-10-02 21:39:02 +0500

    updated footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -32,14 +32,17 @@ const Footer = () => {
 
         <div className="flex items-center md:gap-3 gap-6">
           {socialMedia.map((info) => (
-            <div
-              key={info.id}
-              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+            <a
+               key={info.id}
+               href={info.link}
+               target="_blank"
+               rel="noopener noreferrer"
+               className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75         bg-black-200 rounded-lg border border-black-300"
             >
               <div>
-              <Image src={info.img} alt="icons"  layout="responsive" width={20} height={20} className="object-contain"/>
-              </div>
-            </div>
+                <Image src={info.img} alt="icons" layout="responsive" width={20} height={20} className="object-contain"/>
+             </div>
+            </a>
           ))}
         </div>
       </div>
--- data/index.ts
+++ data/index.ts
@@ -275,13 +275,11 @@ export const socialMedia = [
   {
     id: 1,
     img: "./git.svg",
+    link: "https://github.com/junaiddevworks"
   },
   {
     id: 2,
-    img: "./twit.svg",
-  },
-  {
-    id: 3,
     img: "./link.svg",
+    link:"https://www.linkedin.com/in/muhammad-junaid-2a1191a4/"
   },
 ];
